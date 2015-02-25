211faeebf411ec437a86265deee24dcd6dd32a52
quyen <ly.kim.quyen@ens.fr>
2015-02-25 13:35:18 +0100

    add

---
+++ myocaml/test.ml
@@ -0,0 +1,107 @@
+let l = [1; 2; 3]
+          
+let test = List.rev (List.fold_right (fun x acc -> [x]@acc) l [])
+
+let print_test = List.iter (Printf.printf "%i\n") test
+
+let test_left = List.fold_left (fun x acc -> x + acc) 0 l
+
+(*Union find*)
+(*
+type union_find = {id: int arry;
+                   size: int arry}
+
+let create_union n = 
+  { id = Array.init n (fun i -> i);
+    size = Array.init n (fun i -> 1)
+  }
+
+let union {id; _} p q  =
+  let (value_p, value_q) = (id.(p), id.(q)) in
+  let rec union_find id i =
+    if i < Array.length id
+    then
+      begin
+        if i != q && id.(i) = value_p
+        then
+          id.(i) <- value_q;
+        union_find id (i+1)
+      end
+    else
+      print_string "end of union \n"
+  in union_find 0
+
+let is_connected u p q = u.(p) = u.(q)*)
+
+(*union find with rank*)
+(*
+type union_find =
+    {id: int array;
+     size: int array}
+
+let create_union n =
+  {
+    id = Array.init n (fun i -> i);
+    size = Array.make n 1
+  }
+
+let find_parent parent i =
+  let rec find j =
+    if parent.(j) = j
+    then j
+    else find parent.(j)
+  in find i
+
+let union_rank {id; size} i j =
+  let parent_i = find_parent id i in
+  let parent_j = find_parent id j in
+  if size.(parent_i) < size.(parent_j)
+  then
+    begin
+      id.(parent_i) <- id.(parent_j);
+      size.(parent_j) <- size.(parent_j) + size.(parent_i)
+    end
+  else
+    begin
+      id.(parent_j) <- id.(parent_i);
+      size.(parent_i) <- size.(parent_i) + size.(parent_j)
+    end
+
+let is_connected_ranked {id; _} i j =
+  (find_parent id i) = (find_parent id j)*)
+
+type union_find =
+    {id: int array;
+     size: int}
+
+let create_union n =
+  {
+    id = Array.init n (fun i -> i);
+    size = n (*FIXME*)
+  }
+
+let find_parent parent i =
+  let rec find j =
+    if parent.(j) = j
+    then j
+    else find parent.(j)
+  in find i
+
+let union_rank {id; size} i j =
+  let parent_i = find_parent id i in
+  let parent_j = find_parent id j in
+  if parent_i < parent_j
+  then
+    begin
+      id.(parent_i) <- id.(parent_j);
+      parent_j = parent_j + parent_i
+    end
+  else
+    begin
+      id.(parent_j) <- id.(parent_i);
+      parent_i = parent_i + parent_j
+    end
+
+let is_connected_ranked {id; _} i j =
+  (find_parent id i) = (find_parent id j)
+
