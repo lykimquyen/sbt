e2af417bd17f7f636d1c2c2e3dceb62510a310e2
lykimquyen <ly.kim.quyen@ens.fr>
2015-02-28 16:02:15 +0100

    add

--- myocaml/main.ml
+++ myocaml/main.ml
@@ -102,16 +102,7 @@ let is_connected_ranked {id; _} i j =
 
 (*compute the length of the int list list*)
   
-let length_list classes =
-  let rec aux classes size =
-    match classes with
-      | [] -> size
-      | c :: classes' -> aux classes' (size + 1)
-  in aux classes 0
-
-let length_list_list classes = List.map length_list classes
-
-let length_list2 list =
+ let length_list list =
   let rec aux count acc = function
     | [] -> []
     | [x] -> (x, count + 1) :: acc
@@ -121,41 +112,58 @@ let length_list2 list =
       else
         aux 0 ((a, count +1) :: acc) t
   in aux 0 [] list
-  
-let length_list_list2 l = List.map length_list2
 
+let length_test lists =
+  List.map (fun list -> list, List.length list) lists		
+       
 (*sorting the length of a list of list *)
 
 let length_sort lists =
   let lists = List.map (fun list -> List.length list, list) lists in
   let lists = List.sort (fun a b -> compare (fst a) (fst b)) lists in
   List.map snd lists
-
-let length_test lists =
-  List.map (fun list -> list, List.length list) lists
-
-let length_sort2 lists =
-  let lists = List.map (fun list -> length_test list, list) lists in
-  let lists = List.sort (fun a b -> compare (fst a) (fst b)) lists in
-  List.map snd lists
-  
+ 
 (****************************************************************************************)
 (*TEST*)
 
 let l = [1; 2; 3]
-let ls = [[0;1;2;3]]
+let ls = [[0;1;2];[3]]
+let ls_no_sort = [[4;5;3];[2;1];[0]]
           
 let list_rev_fold = List.rev (List.fold_right (fun x acc -> [x]@acc) l [])
 
 let list_fold_left = List.fold_left (fun x acc -> x + acc) 0 l
   
-let print_int_list = List.iter (Printf.printf "%i\n")
+let print_int_list = List.iter (fun i -> Printf.printf "%i " i)
 
 let rec print_int_list_list ls =
 	match ls with
 	| [] -> ()
 	| is :: ls' ->
-		let print_is = List.iter (fun i -> Printf.printf "%i \n" i) is in
+		let print_is = print_string "["; List.iter (fun i -> Printf.printf " %i " i) is in
+		print_string "]";
 		print_int_list_list ls'
 		
-let print_test = print_int_list_list ls 
+let print_test = print_int_list_list ls
+
+let rec print_pair pair =
+	match pair with
+	| [] -> ()
+	| (p, i) :: ls ->
+		let _ = print_string "\nList:[ "; print_int_list p; print_string "]\nLength: ";
+		print_int i; print_string "\n"
+    	in
+		print_pair ls
+	 
+let print_length_pair_test =
+  let l = length_test ls in
+  print_pair l
+  
+let print_sort_test =
+ print_string "List no sort: [";
+ print_int_list_list ls_no_sort; print_string "]\n";
+ 
+ let s = length_sort ls_no_sort in
+ print_string "List sorted: [" ;
+ print_int_list_list s; print_string "]\n"
+  
