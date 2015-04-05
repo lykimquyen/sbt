3ed41d0f56b158f46f3e7ef3c0ab60c9b12ac652
lykimquyen <ly.kim.quyen@ens.fr>
2015-04-05 14:33:11 +0200

    add

--- myocaml/make.sh
+++ myocaml/make.sh
@@ -1,4 +1,4 @@
 
-ocamlc -c output.ml union_find.ml main.ml;
-ocamlopt -o main output.ml union_find.ml main.ml
+ocamlc -c output.ml list_ex.ml union_find.ml main.ml;
+ocamlopt -o main output.ml list_ex.ml union_find.ml main.ml
 #ocamlc -i main.ml > main.mli
